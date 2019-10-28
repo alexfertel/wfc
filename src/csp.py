bef5e911261ec8d8d73faa190725f54dd2f240a8
Alexander <alexfertel97@gmail.com>
2019-10-28 15:45:50 -0400

    Fix csp representation

--- src/csp.py
+++ src/csp.py
@@ -11,17 +11,22 @@ class Variable:
         self.domain = domain
         self.mutable_domain = self.domain.copy()
 
+    def __str__(self):
+        return f"var {self.index} - {self.name} - {self.domain} - {self.mutable_domain}"
+
+    def __repr__(self):
+        return self.name
 
 class Arc:
     def __init__(self, head, tail, constraint = None):
         self.head = head
         self.tail = tail
-        self.constraint = constraint if constraint else {}
+        self.constraint = constraint if constraint else []
 
     def __getitem__(self, index):
         if index == 0:
             return self.tail
-        elif index = 1:
+        elif index == 1:
             return self.head
         else:
             raise IndexError
@@ -29,11 +34,16 @@ class Arc:
     def __setitem__(self, index, value):
         if index == 0:
             self.tail = value
-        elif index = 1:
+        elif index == 1:
             self.head = value
         else:
             raise IndexError
 
+    def __str__(self):
+        return f"arc {self.tail} --> {self.head} | {self.constraint}"
+
+    def __repr__(self):
+        return f"({repr(self.tail)} --> {repr(self.head)})"
 
 
 class CSP:
@@ -64,20 +74,17 @@ class CSP:
     def unassigned(self):
         return [var for var in self.variables if not var.value]
 
+    def get_var(self, index):
+        for var in self.variables:
+            if var.index == index:
+                return var
+        raise Exception("Var not found!")
 
-def backtracking_search(csp: CSP, SUV = mrv, ODV = lcv):
-    def backtrack(csp: CSP):
-        if csp.is_complete():
-            return csp.current_assignment()
-        
-        var = SUV(csp)
-
-        for value in var.mutable_domain:
-            # if value is consistent with assignment then
-            # What is the above????
-
-
-    return backtrack({}, csp)
+    def get_arc(self, i, j):
+        for arc in self.arcs:
+            if arc.tail.index == i and arc.head.index == j:
+                return arc
+        raise Exception("Arc not found!")
 
 
 # def forward_chekcing(csp):
@@ -95,36 +102,48 @@ def lcv(csp: CSP, var: int):
 
 
 def ac3(csp: CSP):
-    q = list(csp.arcs)
+    q = deepcopy(csp.arcs)
 
     if q:
-        (i, j) = q.pop()
+        arc = q.pop()
         
-        if revise(csp, (i, j)):
-            if not csp.domains[i]: return False
+        if revise(csp, arc):
+            if not len(arc.tail.mutable_domain): return False
 
-            for k in [k for (l, k) in csp.arcs if l == i and k != j]:
-                q.append((k, i))
+            for neighbour in csp.neighbours(arc.tail):
+                if neighbour.index != arc.head.index:
+                    q.append(csp.get_arc(neighbour, arc.tail))
     
     return True
 
-def revise(csp: CSP, vars: tuple):
-    (i, j) = vars
+def revise(csp: CSP, arc: Arc):
     revised = False
-    for x in csp.domains[i].copy():
+    for x in arc.tail.mutable_domain.copy():
         # if no value y in D j allows (x ,y) to satisfy the constraint between X i and X j then
-        allowed = csp.constraints[(i, j)]
         satisfies = False
-        for y in csp.domains[j]:
-            if (x, y) in allowed:
+        for y in arc.head.mutable_domain:
+            if (x, y) in arc.constraint:
                 satisfies = True
         
         if not satisfies:
             print((x, y))
-            csp.domains[i].remove(x)
+            arc.tail.mutable_domain.remove(x)
             revised = True
 
     return revised
 
+def backtracking_search(csp: CSP, SUV = mrv, ODV = lcv):
+    def backtrack(csp: CSP):
+        if csp.is_complete():
+            return csp.current_assignment()
+        
+        var = SUV(csp)
+
+        for value in var.mutable_domain:
+            # if value is consistent with assignment then
+            # What is the above????
+            var.value = value
 
 
+
+    return backtrack({}, csp)
--- tests/ac3_tests.py
+++ tests/ac3_tests.py
@@ -1,22 +1,28 @@
-from src.csp import CSP, ac3
+from src.csp import CSP, ac3, Variable, Arc
 
 def map_coloring_test():
     vardict = ['WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T']
-    variables = [i for i in range(7)]
-    domains = [['red', 'green', 'blue'] for i in range(len(variables))]
-    arcs = [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (0, 1), (1, 2), (2, 3), (3, 4)]
-    constraints = {arc : [(x, y) for y in domains[0] for x in domains[0] if x != y] for arc in arcs}
+    vars = []
+    for index, item in enumerate(vardict):
+        vars.append(Variable(item, index, ['red', 'green', 'blue']))
 
-    domains[0] = ['red']
-    print(variables)
-    print(domains)
+    arc_dict = [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (0, 1), (1, 2), (2, 3), (3, 4)]
+    arcs = []
+    for i, j in arc_dict:
+        arcs.append(Arc(vars[i], vars[j], [(x, y) for y in vars[i].domain for x in vars[j].domain if x != y]))
+        arcs.append(Arc(vars[j], vars[i], [(x, y) for y in vars[i].domain for x in vars[j].domain if x != y]))
+
+    # constraints = {arc : [(x, y) for y in domains[0] for x in domains[0] if x != y] for arc in arcs}
+
+    # domains[0] = ['red']
+    print(vars)
+    # print(domains)
     print(arcs)
-    print(constraints)
+    # print(constraints)
 
-    csp = CSP(variables, arcs, domains, constraints)
+    csp = CSP(vars, arcs)
 
     ac3(csp)
 
-    print(csp.domains)
 
 
