24a44e71e19346bf96f5bdc8a9d3270cbdcd0158
Alexander <alexfertel97@gmail.com>
2019-11-06 15:20:58 -0500

    Remove unnecessary csp.py file and map_coloring_test

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 import sys
-from tests.ac3_tests import map_coloring_test
+from tests.ac3_tests import csp_lib_test
 
 def main():
     print("Hi!!!")
 
 def tests():
-        map_coloring_test()
+        # map_coloring_test()
+        csp_lib_test()
 
 if __name__ == "__main__":
     if "--debug" in sys.argv:
--- tests/ac3_tests.py
+++ tests/ac3_tests.py
@@ -1,28 +1,14 @@
-from src.csp import CSP, ac3, Variable, Arc
-
-def map_coloring_test():
+import constraint as csp
+def csp_lib_test():
+    p = csp.Problem()
     vardict = ['WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T']
-    vars = []
-    for index, item in enumerate(vardict):
-        vars.append(Variable(item, index, ['red', 'green', 'blue']))
-
+    p.addVariables(vardict, ['red', 'green', 'blue'])
+    
     arc_dict = [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (0, 1), (1, 2), (2, 3), (3, 4)]
-    arcs = []
-    for i, j in arc_dict:
-        arcs.append(Arc(vars[i], vars[j], [(x, y) for y in vars[i].domain for x in vars[j].domain if x != y]))
-        arcs.append(Arc(vars[j], vars[i], [(x, y) for y in vars[i].domain for x in vars[j].domain if x != y]))
+    for x, y in arc_dict:
+        p.addConstraint(lambda a, b: a != b, [vardict[x], vardict[y]])
 
-    # constraints = {arc : [(x, y) for y in domains[0] for x in domains[0] if x != y] for arc in arcs}
-
-    # domains[0] = ['red']
-    print(vars)
-    # print(domains)
-    print(arcs)
-    # print(constraints)
-
-    csp = CSP(vars, arcs)
-
-    ac3(csp)
-
+    from pprint import pprint
+    pprint(len(p.getSolutions()))
 
 
