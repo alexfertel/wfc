54945c5aa8c74f89ccae2eb67d6edd85c359f551
Alexander Gonzalez <alexfertel97@gmail.com>
2020-03-06 20:13:55 -0500

    Fix setup method for the validator

--- src/validators/sk.py
+++ src/validators/sk.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 from .validator import Validator
 from ..utils import compatible, dirs, d2s
-
+from sklearn.tree import DecisionTreeClassifier
 
 class MonsterValidator(Validator):
     def __init__(self, patterns):
@@ -11,10 +11,10 @@ class MonsterValidator(Validator):
         self.patterns = patterns
         self.adjacency_rules = defaultdict(list)
 
-        # self.learn_adjacencies()
-
         # Fit the model
-        self.clf = self.setup(patterns)
+        self.clf = self.setup(map(lambda p: p.matrix, patterns))
+
+        self.learn_adjacencies()
 
     def setup(self, patterns):
         ps = [p.flatten() for p in patterns]
@@ -23,8 +23,8 @@ class MonsterValidator(Validator):
         for x in ps:
             for y in ps:
                 for d in dirs:
-                    X.append((x, y, np.array(d2s(d))))
-                    y.append(compatible(x, y, d))
+                    X.append(np.concatenate(x, y, np.array(d2s(d))))
+                    y.append(1 if compatible(x, y, d) else 0)
 
 
         # y = [vector[0] for vector in X]
@@ -34,9 +34,9 @@ class MonsterValidator(Validator):
         # clf = GaussianNB()
         # clf = KNeighborsClassifier(3)
         # clf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)
-        clf = AdaBoostClassifier()
+        clf = DecisionTreeClassifier()
         clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
+        # score = clf.score(X_test, y_test)
 
         return clf
 
@@ -46,7 +46,9 @@ class MonsterValidator(Validator):
             for p2 in self.patterns:
                 for x, y in dirs:
                     d = (x, y)
-                    if compatible(p1.matrix, p2.matrix, d):
+
+                    vector = np.concatenate(p1, p2, np.array(d2s(d)))
+                    if self.clf.predict([vector])[0]:
                         self.adjacency_rules[(p1.index, d)].append(p2.index)
                         self.adjacency_rules[(p2.index, (-x, -y))].append(p1.index)
         pprint(self.adjacency_rules)
@@ -54,3 +56,4 @@ class MonsterValidator(Validator):
 
     def valid_adjacencies(self, identifier, direction):
         return self.adjacency_rules[identifier, direction]
+
