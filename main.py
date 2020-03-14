9f08ddfbcbbfd63601cffeaae88e35f9b12c2c95
Alexander Gonzalez <alexfertel97@gmail.com>
2020-03-14 13:09:36 -0400

    Refactor CLI, add simple support for negative and positive images

--- main.py
+++ main.py
@@ -11,39 +11,45 @@ from src.utils import find
 
 def main():
     # create the top-level parser
-    parser = argparse.ArgumentParser(prog="wfc")
-    subparsers = parser.add_subparsers()
+    main_parser = argparse.ArgumentParser(prog="wfc")
+    subparsers = main_parser.add_subparsers()
+    main_parser.add_argument('-s', '--size', nargs=2, type=int, default=(28, 28),
+                             help='Tuple representing the size of the output image.',
+                             dest='size')
+    main_parser.add_argument('--rotate', action='store_true',
+                               help="Allow rotations.", dest='rotate')
+    main_parser.add_argument('--reflect', action='store_true',
+                               help="Allow reflection.", dest='reflect')
+    main_parser.add_argument('-r', '--renderer', default=None,
+                               help='Renderer to use.', dest='renderer')
+    main_parser.add_argument('-v', '--validator', default=None,
+                               help='Validator to use.', dest='validator')
+    main_parser.add_argument('-q', '--quiet', action='store_true',
+                               help="Don't compute each step.", dest='quiet')
 
     # create the parser for the "simple" command
     simple_parser = subparsers.add_parser('simple')
-    default = 'Flowers1'
-    simple_parser.add_argument('name', default=default,
-                        help='Sample name.')
+    simple_parser.add_argument('name', default='Flowers1',
+                               help='Sample name.')
     simple_parser.add_argument('-n', type=int, default=3,
-                        help="Size of a pattern side.", dest='N')
-    simple_parser.add_argument('-s', '--size', nargs=2, type=int, default=(28, 28),
-                        help='Tuple representing the size of the output image.',
-                        dest='size')
-    simple_parser.add_argument('-r', '--renderer', default=None,
-                        help='Renderer to use.', dest='renderer')
-    simple_parser.add_argument('-v', '--validator', default=None,
-                        help='Validator to use.', dest='validator')
-    simple_parser.add_argument('-q', '--quiet', action='store_true',
-                        help="Don't compute each step.", dest='quiet')
-    simple_parser.add_argument('--rotate', action='store_true',
-                        help="Allow rotations.", dest='rotate')
+                               help="Size of a pattern side.", dest='N')
     simple_parser.add_argument('-g', '--ground', type=int, default=0,
-                        help='The height in pixels of the ground.',
-                        dest='ground')
+                               help='The height in pixels of the ground.',
+                               dest='ground')
     simple_parser.set_defaults(func=simple)
-
 
     # create the parser for the "dichotomic" command
     dichotomic_parser = subparsers.add_parser('dichotomic')
-    dichotomic_parser.add_argument('z')
+    dichotomic_parser.add_argument('i0', default='Colored City')
+    dichotomic_parser.add_argument('i1', default='Colored City Negative')
+    dichotomic_parser.add_argument('-n0', type=int, default=3,
+                                   help="Size of a pattern side.", dest='N0')
+    dichotomic_parser.add_argument('-n1', type=int, default=3,
+                                   help="Size of a pattern side.", dest='N1')
+
     dichotomic_parser.set_defaults(func=dichotomic)
 
-    args = parser.parse_args()
+    args = main_parser.parse_args()
     args.size = (args.size[0], args.size[1])
 
     # Create the output dir if it doesn't exist
@@ -52,6 +58,7 @@ def main():
         parents=True, exist_ok=True)
 
     args.func(args)
+
 
 def simple(args):
     if args.renderer:
@@ -69,6 +76,7 @@ def simple(args):
     wfc.generate(args.name, args.size, quiet=args.quiet)
 
     wfc.render(args.name)
+
 
 def dichotomic(args):
     pass
